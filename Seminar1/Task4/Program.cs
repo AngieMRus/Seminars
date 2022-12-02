bf8d6f8f48ca328afc145dbd1ac80b7aa4ec59c7
AngelinaM <angelina1990-07@mail.ru>
2022-12-02 03:24:14 +0300

    Task4

---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+if (n >= m && n >= l)
+    Console.WriteLine(n);
+else if (l >= m && l >= n)
+    Console.WriteLine(l);
+else
+    Console.WriteLine(m);
