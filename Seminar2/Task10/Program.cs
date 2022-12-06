9e8a49fe46dc75db521c2c7f46408c3ddabd5190
AngelinaM <angelina1990-07@mail.ru>
2022-12-06 21:00:37 +0300

    Task10 added

---
+++ Seminar2/Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 99 && n < 1000)
+{
+    string s = Convert.ToString(n);
+    Console.WriteLine(s[1]);
+}
+else if (n < -99 && n > -1000)
+{
+    string s = Convert.ToString(n);
+    Console.WriteLine(s[2]);
+}
+else
+    Console.WriteLine("Неверные исходные данные. Повторите попытку.");
