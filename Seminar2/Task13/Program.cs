b3247e111868c504fff8d5befb857a1fa41bcdd2
AngelinaM <angelina1990-07@mail.ru>
2022-12-06 22:00:51 +0300

    Task13 added

---
+++ Seminar2/Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100 && n > -100)
+    Console.WriteLine("Третьей цифры нет."); 
+else if (n > 99 && n < 1000 || n < -99 && n > -1000)
+    Console.WriteLine(n % 10);
+else 
+    {
+    while (n > 999 || n < -999)
+    {
+    n = n / 10;
+    }
+    Console.WriteLine(n % 10);
+    }
