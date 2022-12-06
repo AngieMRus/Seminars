cc6cf72c88e59977af9d9059d038a47849ea5b0e
AngelinaM <angelina1990-07@mail.ru>
2022-12-06 20:17:02 +0300

    Task19 added

---
+++ Seminar3/Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string s = Convert.ToString(n);
+
+char[] ch = s.ToCharArray();
+
+if (ch.Length == 5)
+{
+    if (ch[0] == ch[4] && ch[1] == ch[3])
+        Console.WriteLine("Данное число является палиндромом. Ура!");
+    else
+        Console.WriteLine("Данное число НЕ является палиндромом. Увы!");
+}
+
+else
+    Console.WriteLine("Неверные исходные данные. Повторите попытку.");
