097583ca7e3f04a2dde5eb9d1061d1477578cc3a
AngelinaM <angelina1990-07@mail.ru>
2022-12-06 14:24:06 +0300

    Seminar1 changed

--- Seminar1/Task8/Program.cs
+++ Seminar1/Task8/Program.cs
@@ -3,9 +3,31 @@
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
+
+if (n < 0)
+    {
+    int i = -2;
+    while (i >= n)
+    {
+        Console.Write($"{i} ");
+        i = i - 2;
+    }}
+else
+    {
+    int i = 2;
+    while (i <= n)
+    {
+        Console.Write($"{i} ");
+        i = i + 2;
+    }}
+
+/*
+
 int i = 2;
 while (i <= n)
 {
     Console.Write($"{i} ");
     i = i + 2;
 }
+
+*/
