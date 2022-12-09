25355df820d47f499a8c5bd550361c78e5e040d0
AngelinaM <angelina1990-07@mail.ru>
2022-12-09 19:02:16 +0300

    Seminar4 added

---
+++ Seminar4/Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+double m = Convert.ToInt32(Console.ReadLine());
+
+double result = 1;
+
+for (int count = 0; count < m; count++)
+    result = result * n;
+
+Console.WriteLine($"{n} ^ {m} = {result}");
