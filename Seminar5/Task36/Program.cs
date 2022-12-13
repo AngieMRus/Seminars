fe41cc7dc2904cdcea849b405dd125e38c0410a9
AngelinaM <angelina1990-07@mail.ru>
2022-12-13 18:31:30 +0300

    Task36 added

---
+++ Seminar5/Task36/Program.cs
@@ -0,0 +1,27 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 101);
+}
+
+
+int ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+        {
+            sum = sum + array[i];
+        }
+    return sum;
+}
+
+
+Console.Clear();
+
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+InputArray(array);
+Console.WriteLine($"Mассив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {ReleaseArray(array)}");
