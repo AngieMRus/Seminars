6b1b517df8db5a5fb59d0a737c1bb5e8e27ca20a
AngelinaM <angelina1990-07@mail.ru>
2022-12-13 20:37:04 +0300

    Task38 modified

--- Seminar5/Task38/Program.cs
+++ Seminar5/Task38/Program.cs
@@ -5,37 +5,13 @@
 }
 
 
-int Max (int arg1, int arg2, int arg3)
-{
-    int result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
-    return result;
-}
-
-int a1 = array;
-int b1 = 23;
-int c1 = 39;
-int a2 = 12;
-int b2 = 2311;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23111;
-int c3 = 313;
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
-
-int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
-
-Console.WriteLine(max);
-
-
 Console.Clear();
 
 int[] array = new int[10];
 InputArray(array);
 Console.WriteLine($"Mассив: [{string.Join(", ", array)}]");
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {ReleaseArray(array)}");
+
+Console.WriteLine($"Максимальный элемент массива: " + array.Max());
+Console.WriteLine($"Минимальный элемент массива: " + array.Min());
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {array.Max() - array.Min()}");
