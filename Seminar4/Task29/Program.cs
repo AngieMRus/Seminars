ca63891021c86dab3c158ec1421edd03488ed680
AngelinaM <angelina1990-07@mail.ru>
2022-12-13 19:56:50 +0300

    Task29 added

---
+++ Seminar4/Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+if (n > 0)
+{
+    for (int count = 0; n > 0; count++)
+        sum = sum + n % 10;
+    Console.WriteLine(sum);
+}
+
+else if (n < 0)
+{
+    for (int count = 0; n < 0; count++)
+        sum = (sum + n % 10) * (-1);
+    Console.WriteLine(sum);
+}
+
+else Console.WriteLine("0");
