de7e672c426b408ade06a52b1dbb72bb9dd72ad4
AngelinaM <angelina1990-07@mail.ru>
2022-12-14 23:06:04 +0300

    Task36 modified

--- Seminar5/Task36/Program.cs
+++ Seminar5/Task36/Program.cs
@@ -1,26 +1,17 @@
 ﻿void InputArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(0, 101);
-}
-
-
-int ReleaseArray(int[] array)
-{
-    int sum = 0;
-    for (int i = 1; i < array.Length; i = i + 2)
-        {
-            sum = sum + array[i];
-        }
-    return sum;
+        array[i] = new Random().Next(-100, 101);
 }
 
 
 Console.Clear();
 
-Console.Write("Введите кол-во элементов массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
+int[] array = new int[10];
 InputArray(array);
 Console.WriteLine($"Mассив: [{string.Join(", ", array)}]");
-Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {ReleaseArray(array)}");
+
+Console.WriteLine($"Максимальный элемент массива: " + array.Max());
+Console.WriteLine($"Минимальный элемент массива: " + array.Min());
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {array.Max() - array.Min()}");
