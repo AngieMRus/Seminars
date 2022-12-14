27f41f3e37c665755c738145bffd740431300f40
AngelinaM <angelina1990-07@mail.ru>
2022-12-14 20:53:32 +0300

    Task27 modified

--- Seminar4/Task27/Program.cs
+++ Seminar4/Task27/Program.cs
@@ -2,11 +2,14 @@
 
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
+
 int sum = 0;
 int count = 0;
-while (n >= 10)
+
+while (n > 0 || n < 0)
 {
     sum = sum + n % 10;
     count++;
 }
+
 Console.WriteLine(sum);
