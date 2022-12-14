73ba23180a80a10336ef5e1882aed7171cd204a1
AngelinaM <angelina1990-07@mail.ru>
2022-12-14 20:29:00 +0300

    Task27 added

--- Seminar4/Task27/Program.cs
+++ Seminar4/Task27/Program.cs
@@ -2,14 +2,11 @@
 
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
-string s = Convert.ToString(n);
-
-char[] arr = s.ToCharArray();
-
 int sum = 0;
-
-for (int count = 0; count < arr.Length; count++)
-    sum = sum + arr[count];
-    
+int count = 0;
+while (n >= 10)
+{
+    sum = sum + n % 10;
+    count++;
+}
 Console.WriteLine(sum);
