708ff9bd523c0931e657ab212847701204bd58e7
AngelinaM <angelina1990-07@mail.ru>
2022-12-16 11:53:01 +0300

    Task27 modified

--- Seminar4/Task27/Program.cs
+++ Seminar4/Task27/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Clear();
 
 Console.Write("Введите число: ");
-double n = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
 
-double sum = 0;
+int sum = 0;
 int count = 0;
 
 while (n > 0 || n < 0)
@@ -12,4 +12,4 @@ while (n > 0 || n < 0)
     count++;
 }
 
-Console.WriteLine(sum);
+Console.WriteLine($"Сумма цифр в исходном числе: {sum}");
