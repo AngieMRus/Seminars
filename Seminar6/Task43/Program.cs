b747d2851adf533884a700b57f9b4d84dee1c3a5
AngelinaM <angelina1990-07@mail.ru>
2022-12-19 23:15:23 +0300

    Task43 added

---
+++ Seminar6/Task43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите значение переменной b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение переменной k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение переменной b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение переменной k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double intersection = (b2 - b1) / (k1 - k2);
+
+Console.WriteLine($"Точка пересечения двух прямых: {intersection}");
