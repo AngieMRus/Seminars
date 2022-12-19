aebf0629bd3bd87e404f7c18a4a1c0c216319df2
AngelinaM <angelina1990-07@mail.ru>
2022-12-19 22:34:55 +0300

    Task41 added

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите любые числа через пробелы, а затем нажмите клавишу Enter: ");
+int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int sum = 0;
+
+for (int count = 0; count < numbers.Length; count++)
+{
+    if (numbers[count] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine(sum);
