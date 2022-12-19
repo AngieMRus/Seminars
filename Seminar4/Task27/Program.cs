787c9d655332aff292772c193c09b1447b412c9c
AngelinaM <angelina1990-07@mail.ru>
2022-12-20 00:04:29 +0300

    Task27 added and modified

--- Seminar4/Task27/Program.cs
+++ Seminar4/Task27/Program.cs
@@ -1,11 +1,25 @@
 ﻿Console.Clear();
 
 Console.Write("Введите число: ");
-double n = Convert.ToInt32(Console.ReadLine());
-double sum = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
 
-while (n >= 1 || n <= (-1))
+if (n > 0)
 {
-    sum += n % 10;
+    while (n >= 1)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
     Console.WriteLine($"Сумма цифр в исходном числе: {sum}");
 }
+else if (n < 0)
+{
+    while (n <= (-1))
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    Console.WriteLine($"Сумма цифр в исходном числе: {sum * (-1)}");
+}
+else Console.WriteLine("0. Повторите попытку.");
