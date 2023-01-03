97f9db0f351d65d3970e29df690e89eef8b211c4
AngelinaM <angelina1990-07@mail.ru>
2023-01-03 17:50:46 +0300

    Task68 added

---
+++ Seminar9/Task68/Program.cs
@@ -0,0 +1,17 @@
+﻿int rec(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    else
+    if ((a > 0) && (b == 0))
+      return rec(a - 1, 1);
+    else
+      return rec(a - 1, rec(a, b - 1));
+}
+
+
+Console.Write("Введите число > 0: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число > 0: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
